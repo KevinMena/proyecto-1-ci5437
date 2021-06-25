8610407544090c36cbb8b78938f794e42358e3cc
Kevin Mena <kevin_mena3@hotmail.com>
2021-06-24 22:58:45 -0400

    Agregados pdbs de hanoi 14 y 18, arreglos menores

--- psvn-for-ci5437/hanoi4p12d/README.md
+++ psvn-for-ci5437/hanoi4p12d/README.md
@@ -2,15 +2,15 @@
 
 Para compilar los pdb, se corren los comando  
 ```
-make 14x4_topspin/abst_1.pdb
-make 14x4_topspin/abst_2.pdb
-make 14x4_topspin/abst_3.pdb
-make 14x4_topspin.ida_star
-make 14x4_topspin.a_star
+make -f make_pdb.mk hanoi4p12d/abst_1.pdb
+make -f make_pdb.mk hanoi4p12d/abst_2.pdb
+make -f make_pdb.mk hanoi4p12d/abst_3.pdb
+make -f make_solver.mk hanoi4p12d.ida_star
+make -f make_solver.mk hanoi4p12d.a_star
 ```
 
 ## Correr el algoritmo
 
 ```
-./14x4_topspin.<algoritmo> <path_archivo_entrada> <path_archivo_salida>
+./hanoi4p12d.<algoritmo> <path_archivo_entrada> <path_archivo_salida>
 ```
---
+++ psvn-for-ci5437/hanoi4p18d/make_pdb.mk
@@ -0,0 +1,49 @@
+CC = gcc
+CXX = g++
+OPT = -Wall -O3 -Wno-unused-function -Wno-unused-variable
+PSVNOPT = --state_map --backward_moves --fwd_history_len=0 --bwd_history_len=0
+
+psvn2c_core.c:
+	cp ../src/psvn2c_core.c ./psvn2c_core.c
+
+psvn2c_state_map.c:
+	cp ../src/psvn2c_state_map.c ./psvn2c_state_map.c
+
+psvn2c_abstraction.c:
+	cp ../src/psvn2c_abstraction.c ./psvn2c_abstraction.c
+
+%.c: %.psvn psvn2c_core.c psvn2c_state_map.c psvn2c_abstraction.c
+	../bin/psvn2c $(PSVNOPT) --name=$(*F) < $< > $@
+	rm -f ./psvn2c_core.c ./psvn2c_state_map.c ./psvn2c_abstraction.c
+
+.PRECIOUS: %.c
+
+%.distSummary: %.c ../global/distSummary.cpp
+	@echo "**** Constructing $@ ..."
+	@$(CXX) $(OPT) ../global/distSummary.cpp -include $< -o $@
+	@rm -f $*.c
+
+abstractor:
+	@echo "**** Constructing abstractor ..."
+	@$(CXX) $(OPT) ../src/abstractor.cpp ../src/psvn.cpp -o $@
+
+%.pdb: abstractor
+	@rm -f `basename $*`.abst `basename $*`.pdb `basename $*`.psvn
+	@echo "**** Running abstractor on `dirname $*`.psvn with `basename $*`.txt ..."
+	@./abstractor `dirname $*`.psvn `basename $*` < `basename $*`.txt > /dev/null
+	@make -f make_pdb.mk `basename $*`.distSummary
+	@echo "**** Constructing PDB `basename $*`.pdb ..."
+	@./`basename $*`.distSummary `basename $*`.pdb
+	@rm -f abstractor `basename $*`.distSummary
+	
+%.a_star: %.c ../global/a_star.cpp
+	$(CXX) $(OPT) ../global/a_star.cpp -include $< -include hanoi_18_pdb.cpp -o $@
+	rm -f $*.c
+
+%.ida_star: %.c ../global/ida_star.cpp
+	$(CXX) $(OPT) ../global/ida_star.cpp -include $< -include hanoi_18_pdb.cpp -o $@
+	rm -f $*.c
+
+.PHONY: clean
+clean:
+	rm -fr *.succ psvn2c_core.c psvn2c_state_map.c psvn2c_abstraction.c *.dSYM *.o *~
